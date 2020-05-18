8df5044941f45d3da8a87d1d750bc7868652cbbd
Eugene Smirnov <zheka2304@gmail.com>
2020-05-19 00:38:41 +0300

    added menu

--- lab1/main.cpp
+++ lab1/main.cpp
@@ -4,13 +4,53 @@
 
 
 int main() {
+    rb_map<std::string, std::string> map;
 
-    rb_map<std::string, std::string> mMap;
-    mMap["a"] = "b";
-    mMap["b"] = "c";
-    mMap["b"] = "a";
-    mMap.remove("b");
-    mMap.print();
+    while (true) {
+        std::cout << "\n\nmap: ";
+        map.print();
+        std::cout << "\n\ntype 1 to insert value\ntype 2 to remove value\ntype 3 to get value\ntype 4 to debug red-black tree\ntype any other number to exit\n";
 
-    return 0;
+        int choice = 1;
+        std::cin >> choice;
+        switch (choice) {
+            case 1: {
+                std::string key, value;
+                std::cout << "input key: ";
+                std::cin >> key;
+                std::cout << "input value: ";
+                std::cin >> value;
+                map[key] = value;
+                break;
+            }
+            case 2: {
+                std::string key, value;
+                std::cout << "input key: ";
+                std::cin >> key;
+                if (map.remove(key)) {
+                    std::cout << "value was removed\n";
+                } else {
+                    std::cout << "no such key\n";
+                }
+                break;
+            }
+            case 3: {
+                std::string key, value;
+                std::cout << "input key: ";
+                std::cin >> key;
+                if (map.has(key)) {
+                    std::cout << "value at " << key << " = " << map[key] << "\n";
+                } else {
+                    std::cout << "no such key\n";
+                }
+                break;
+            }
+            case 4: {
+                map.show_tree();
+                break;
+            }
+            default:
+                return 0;
+        }
+    }
 }
